559bec5070bea17c9bccb1763a6e5aa462e0dfcb
jj22511-dev <jj22511@gmail.com>
2023-10-29 15:09:21 +0800

    add adjust settings panel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ import { potrace, init } from 'esm-potrace-wasm';
 function App() {
   const [rasterImage, setRasterImage] = createSignal("");
   const [vectorImage, setVectorImage] = createSignal("");
+  const [portraceSettings, setPortraceSetting] = createSignal({
+    pathonly: false, // default false
+    extractcolors: true, // default true
+
+    turdsize: 2, // default 2
+    turnpolicy: 4, // default 4
+    alphamax: 1, // default 1
+    opticurve: 1, // default 1
+    opttolerance: 0.2, // default 0.2
+    posterizelevel: 1, // [1, 255] // default 2
+    posterizationalgorithm: 0, // 0: simple, 1: interpolation default 0
+  });
 
   onMount(async() => {
     await init();
@@ -13,26 +25,31 @@ function App() {
     if (!e.target) return;
       const file_image = e.target.files[0];
       const imgUrl = URL.createObjectURL(file_image);
-      const blob = await fetch(imgUrl).then(res=>res.blob());
-
       setRasterImage(imgUrl);
 
-      const svg = await potrace(
-        blob,
-        {
-          pathonly: false, // default false
-          extractcolors: true, // default true
-          
-          turdsize: 2, // default 2
-          turnpolicy: 4, // default 4
-          alphamax: 1, // default 1
-          opticurve: 1, // default 1
-          opttolerance: 0.2, // default 0.2
-          posterizelevel: 1, // [1, 255] // default 2
-          posterizationalgorithm: 0, // 0: simple, 1: interpolation default 0
-        }
-      );
+      let blob = await fileToBlob(imgUrl);
+      const svg = await convertToSvg(blob);
+      setVectorImage(svg);
+  }
 
+  const fileToBlob = async (imageUrl: string) => fetch(imageUrl).then(res => res.blob());
+
+  const convertToSvg = async (imageUrl: ImageBitmapSource) => {
+    const svg = await potrace(imageUrl, portraceSettings())
+    return svg;
+  }
+
+  const adjustPortraceSettings = async (prop: string, value: number | boolean) => {
+    setPortraceSetting({
+        ...portraceSettings(),
+        [prop]: value 
+    });
+
+      console.log("settings changed", { [prop]: value  })
+      if (!rasterImage()) return;
+
+      let blob = await fileToBlob(rasterImage());
+      const svg = await convertToSvg(blob);
       setVectorImage(svg);
   }
 
@@ -60,38 +77,64 @@ function App() {
         }
 
       </div> 
-      <div class="bg-slate-100 p-5">
-        <h3>Adjust Vector Image</h3>
 
-        <div>
-          <label class='block'>turdsize: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
+      { vectorImage() && (
+        <div class="bg-slate-100 p-5 w-[500px]">
+          <div class="">
+            <p class="font-extrabold">Adjust Vector Image</p>
+          </div>
+
+          <div class="grid grid-cols-2">
+            <label class='block'>turdsize: [{portraceSettings().turdsize}]</label>
+            <input type="range" min="1" max="100"
+              value={portraceSettings().turdsize}
+              onChange={ (e) => adjustPortraceSettings('turdsize', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">turnpolicy: [{portraceSettings().turnpolicy}]</label>
+            <input type="range" min="1" max="100"
+              value={portraceSettings().turnpolicy}
+              onChange={ (e) => adjustPortraceSettings('turnpolicy', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">alphamax: [{portraceSettings().alphamax}]</label>
+            <input type="range" min="0" max="2"
+              value={portraceSettings().alphamax}
+              onChange={ (e) => adjustPortraceSettings('alphamax', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">opticurve: [{portraceSettings().opticurve}]</label>
+            <input type="range" min="1" max="100"
+              value={portraceSettings().opticurve}
+              onChange={ (e) => adjustPortraceSettings('opticurve', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">opttolerance: [{portraceSettings().opttolerance}]</label>
+            <input type="range" min="1" max="100"
+              value={portraceSettings().opttolerance}
+              onChange={ (e) => adjustPortraceSettings('opttolerance', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">posterizelevel: [{portraceSettings().posterizelevel}]</label>
+            <input type="range" min="1" max="20"
+              value={portraceSettings().posterizelevel}
+              onChange={ (e) => adjustPortraceSettings('posterizelevel', e.target.valueAsNumber)}
+            />
+          </div>
+          <div class="grid grid-cols-2">
+            <label class="block">posterizationalgorithm: [{portraceSettings().posterizationalgorithm}] </label>
+            <input type="range" min="0" max="1"
+              value={portraceSettings().posterizationalgorithm}
+              onChange={ (e) => adjustPortraceSettings('posterizationalgorithm', e.target.valueAsNumber)}
+            />
+          </div>
         </div>
-        <div class="">
-          <label class="block">turnpolicy: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-        <div class="">
-          <label class="block">alphamax: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-        <div class="">
-          <label class="block">opticurve: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-        <div class="">
-          <label class="block">opttolerance: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-        <div class="">
-          <label class="block">posterizelevel: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-        <div class="">
-          <label class="block">posterizationalgorithm: </label>
-          <input class="w-1/4" type="range" min="1" max="100" value="50" />
-        </div>
-      </div>
+      )}
     </div>
   )
 }
