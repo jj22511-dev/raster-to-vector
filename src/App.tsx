8b222ccfe23f0af0ff5dfe860b5f0bf1edc6589d
jj22511-dev <jj22511@gmail.com>
2023-10-29 15:27:57 +0800

    fix input event type

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,9 @@ function App() {
     await init();
   })
 
-  const handleFile = async (e: Event) => {
-    if (!e.target) return;
-      const file_image = e.target.files[0];
+  const handleFile = async (e: HTMLInputElement) => {
+    if (!e?.files) return;
+      const file_image = e.files[0];
       const imgUrl = URL.createObjectURL(file_image);
       setRasterImage(imgUrl);
 
@@ -56,7 +56,7 @@ function App() {
   return (
     <div>
       <div class="p-2 w-screen bg-gray-400">
-        <input onChange={handleFile}
+        <input onChange={(e) => handleFile(e.target)}
           accept='image/*'
           class="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file" />
       </div> 
